8b276800b9de7557858389bca02d956c14129fff
Petar Metodiev <petar.metodiev@gmail.com>
2018-11-19 20:56:53 +0200

    Use express instead of native http module

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,20 @@
-import {
-    default as http,
-    IncomingMessage,
-    ServerResponse
-} from 'http';
+import express from 'express';
+
 import { routes } from './routes';
+import {
+	NextFunction,
+	Response,
+	Request
+} from 'express-serve-static-core';
 
 const anounceOpenPort = (port: number) => () => console.log(`Listening on port ${port}`);
-
 const port = 3000;
 
-const server = http.createServer(routes);
-server.listen(port, anounceOpenPort(port));
+const app = express();
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+	console.log('hi!');
+	return next();
+})
+
+app.listen(port, anounceOpenPort(port));
