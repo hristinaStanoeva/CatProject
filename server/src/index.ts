de7db8716a96636882b16df634ab62a23e047855
Petar Metodiev <petar.metodiev@gmail.com>
2019-02-07 22:06:03 +0200

    Cleanup and use new routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,49 +8,15 @@ import {
 
 import { path } from './util/path';
 
-import {
-    CustomRequest,
-} from './models';
-
-import sampleRoutes from './routes/sample';
+import listsRoutes from './routes/lists.routes';
 
 const anounceOpenPort = (port: number) => () => console.log(`Listening on port ${port}`);
 const appPort = 3000;
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.use((req: CustomRequest, res, next) => {
-    req.customData = req.customData || {};
-
-    req.customData = {
-        ...req.customData,
-        firstMiddleware: true
-    };
-
-    return next();
-});
-
-app.use((req: CustomRequest, res, next) => {
-    req.customData = req.customData || {};
-
-    req.customData = {
-        ...req.customData,
-        secondMiddleware: true
-    };
-
-    return next();
-});
-
-app.use('/api/', sampleRoutes);
-
-app.get('/home', (req, res, next) => {
-    res.sendFile(join(path.projectRoot, 'views', 'home.html'));
-});
-
-app.use((req, res, next) => {
-    res.sendStatus(404);
-});
+app.use('/api', listsRoutes);
 
 app.listen(appPort, anounceOpenPort(appPort));
