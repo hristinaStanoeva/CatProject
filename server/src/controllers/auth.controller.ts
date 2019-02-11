80a7e429eb59de556a40628d424542a445168758
Petar Metodiev <petar.metodiev@gmail.com>
2019-02-11 19:33:51 +0200

    Add sample validation for reset password route

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,4 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
+import { validationResult } from 'express-validator/check';
 
 interface CustomBodyRequest<K extends keyof any> extends Request {
     body: Partial<Record<K, string>>;
@@ -7,6 +8,13 @@ interface CustomBodyRequest<K extends keyof any> extends Request {
 export type LoginRequest = CustomBodyRequest<'email' | 'password'>;
 export type RegisterRequest = CustomBodyRequest<'email' | 'password' | 'confirmPassword'>;
 export type ResetPasswordRequest = CustomBodyRequest<'email'>;
+
+interface ValidationResult {
+    location: Location;
+    param: string;
+    msg: any;
+    value: any;
+}
 
 export const loginUser = (req: LoginRequest, res: Response, next: NextFunction) => {
     res.json('User logged in!');
@@ -17,5 +25,12 @@ export const registerUser = (req: RegisterRequest, res: Response, next: NextFunc
 };
 
 export const resetUserPassword = (req: ResetPasswordRequest, res: Response, next: NextFunction) => {
-    res.json('Password reset!');
+    if (validationResult(req).isEmpty()) {
+        return res.sendStatus(200);
+    } else {
+        const result = validationResult<ValidationResult>(req)
+            .array()
+            .map(({ msg, param }) => ({ field: param, error: msg }));
+        return res.status(400).json(result);
+    }
 };
--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,8 +1,12 @@
 import { Router } from 'express';
+
+import { body } from 'express-validator/check';
+
 import {
     loginUser,
     registerUser,
-    resetUserPassword
+    resetUserPassword,
+    ResetPasswordRequest
 } from '../controllers';
 
 const router = Router();
@@ -11,6 +15,17 @@ router.post('/login', loginUser);
 
 router.post('/register', registerUser);
 
-router.post('/reset-password', resetUserPassword);
+router.post('/reset-password', [
+    body('email')
+        .isEmail()
+        .withMessage('Invalid email address')
+        .custom((value: string, { req }: { req: ResetPasswordRequest }) => {
+            if (true) {
+                return Promise.reject('No such user: ' + req.body.email);
+            }
+            return Promise.resolve();
+        })
+        .normalizeEmail()
+], resetUserPassword);
 
 export default router;
