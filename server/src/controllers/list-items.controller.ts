afd636e25ac62352eadb663cb2177dcf6ff3d9cb
Petar Metodiev <petar.metodiev@gmail.com>
2019-02-11 16:59:49 +0200

    Add missing semicolons

--- server/src/controllers/list-items.controller.ts
+++ server/src/controllers/list-items.controller.ts
@@ -6,7 +6,7 @@ export const getAllListItems = (req: Request, res: Response, next: NextFunction)
 
 export const createListItem = (req: Request, res: Response, next: NextFunction) => {
     res.json('Created list item');
-}
+};
 
 export const updateListItem = (req: Request, res: Response, next: NextFunction) => {
     res.json('Updated list item with id ' + req.params.id);
@@ -14,4 +14,4 @@ export const updateListItem = (req: Request, res: Response, next: NextFunction) 
 
 export const deleteListItemById = (req: Request, res: Response, next: NextFunction) => {
     res.json('Deleted list item by id ' + req.params.id);
-}
+};
