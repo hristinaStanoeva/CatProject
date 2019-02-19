39c9a05f282d88b49c551a84234ce09d8257d73c
Petar Metodiev <petar.metodiev@gmail.com>
2019-02-19 16:55:25 +0200

    Refactor validators

--- server/src/routes/api/auth.routes.ts
+++ server/src/routes/api/auth.routes.ts
@@ -14,11 +14,18 @@ import { runValidators } from '../../middlewares/runValidators.middleware';
 
 const router = Router();
 
+const emailValidator = () => body('email')
+    .isEmail()
+    .normalizeEmail()
+    .withMessage('Invalid email address');
+
+const passwordValidator = () => body('password')
+        .trim()
+        .isLength({ min: 8 })
+        .withMessage('Password has to be at least 8 characters long');
+
 router.post('/login', [
-    body('email')
-        .normalizeEmail()
-        .isEmail()
-        .withMessage('Invalid email address')
+    emailValidator()
         // move to a separate middleware to check db data
         .custom((value: string, { req }: { req: LoginRequest }) => {
             if (false) {
@@ -26,10 +33,8 @@ router.post('/login', [
             }
             return Promise.resolve();
         }),
-    body('password')
-        .trim()
-        .isLength({ min: 8 })
-        .withMessage('Password has to be at least 8 characters long')
+    passwordValidator()
+        // maybe move check for password in db in a separate middleware
         .custom((value: string, { req }: { req: LoginRequest }) => {
             if (false) {
                 return Promise.reject('Wrong password');
@@ -42,21 +47,15 @@ router.post('/login', [
 ], loginUser);
 
 router.post('/register', [
-    body('email')
-        .isEmail()
-        .withMessage('Invalid email address')
+    emailValidator()
         // maybe move to a separete middleware for checking if the email is in the db
         .custom((value: string, { req }: { req: RegisterRequest }) => {
             if (false) {
                 return Promise.reject(`Email ${req.body.email} already exists`);
             }
             return Promise.resolve();
-        })
-        .normalizeEmail(),
-    body('password')
-        .trim()
-        .isLength({ min: 8 })
-        .withMessage('Password has to be at least 8 characters long')
+        }),
+    passwordValidator()
         .custom((value: string, { req }: { req: RegisterRequest }) => {
             if (value !== req.body.confirmPassword) {
                 return Promise.reject('Passwords must match');
@@ -67,17 +66,14 @@ router.post('/register', [
 ], registerUser);
 
 router.post('/reset-password', [
-    body('email')
-        .isEmail()
-        .withMessage('Invalid email address')
+    emailValidator()
         // maybe remove this check as it is bad security practise
         .custom((value: string, { req }: { req: ResetPasswordRequest }) => {
             if (false) {
                 return Promise.reject('No such user: ' + req.body.email);
             }
             return Promise.resolve();
-        })
-        .normalizeEmail(),
+        }),
     runValidators
 ], resetUserPassword);
 
