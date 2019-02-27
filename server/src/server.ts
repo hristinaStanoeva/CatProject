a940a799674e06f6d0a38bf3a2d6adeeec36a7b8
Petar Metodiev <petar.metodiev@gmail.com>
2019-02-27 08:41:18 +0200

    Connect user model to db and create dummy user on start

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,14 +5,21 @@ import { envFile } from './util/path';
 dotenv.config({ path: envFile });
 
 import app from './app';
-import { sequelize } from './util/database';
+import { db } from './util/database';
 
 const anounceOpenPort = (port: number | string) => () =>
     console.log(`Listening on port ${port}`);
 const appPort = process.env.PORT || 3000;
 
-sequelize
+db.sequelize
     .sync({ force: true })
+    .then(() =>
+        db.User.create({
+            email: 'test@mail.com',
+            password:
+                '123456789012345678901234567890123456789012345678901234567890',
+        })
+    )
     .then(res => {
         console.log('Connected!');
         app.listen(appPort, anounceOpenPort(appPort));
--- server/src/util/database.ts
+++ server/src/util/database.ts
@@ -1,3 +1,18 @@
 import Sequelize from 'sequelize';
+import { UserInstance, UserAttributes, UserFactory } from '../schemas/user.schema';
 
-export const sequelize = new Sequelize(process.env.DB_URL, { operatorsAliases: false });
+export interface Db {
+    sequelize: Sequelize.Sequelize;
+    Sequelize: Sequelize.SequelizeStatic;
+    User: Sequelize.Model<UserInstance, UserAttributes>;
+}
+
+const sequelize = new Sequelize(process.env.DB_URL, {
+    operatorsAliases: false,
+});
+
+export const db: Db = {
+    sequelize,
+    Sequelize,
+    User: UserFactory(sequelize, Sequelize)
+};
