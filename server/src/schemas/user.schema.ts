e7dd16c0bff2b604a603f497e5080499388a1d38
Petar Metodiev <petar.metodiev@gmail.com>
2019-03-03 02:59:30 +0200

    Add password hashing and some user sanitization

--- server/src/schemas/user.schema.ts
+++ server/src/schemas/user.schema.ts
@@ -1,4 +1,8 @@
 import Sequelize from 'sequelize';
+import { normalizeEmail, trim } from 'validator';
+import { hash } from 'bcrypt';
+import { compose } from 'ramda';
+
 import { BaseAttributes } from '../models/base-attributes.model';
 
 export interface UserAttributes extends BaseAttributes {
@@ -20,35 +24,44 @@ export const UserFactory = (
             type: DataTypes.STRING,
             unique: true,
             allowNull: false,
+            validate: {
+                notEmpty: true,
+                isEmail: true,
+            },
         },
         password: {
             type: DataTypes.STRING(60),
             allowNull: false,
             validate: {
-                isCorrectLength(value: string) {
-                    if (value.length !== 60) {
-                        throw new Error(
-                            'Password hash has to be exactly 60 characters'
-                        );
-                    }
-                },
+                len: [8, 50],
+                isAscii: true,
             },
         },
         image_url: {
             type: DataTypes.STRING,
+            validate: {
+                isUrl: true,
+            },
         },
     });
 
-    // If hashing is done in User.beforeCreate() then the validation is skipped
-    // http://docs.sequelizejs.com/manual/tutorial/hooks.html#order-of-operations
-    User.beforeValidate((user, options) => {
-        // hash here pls
-        return Promise.resolve(
-            user.password
-                .split('')
-                .reverse()
-                .join('')
-        ).then(pw => (user.password = pw));
+    User.beforeValidate(user => {
+        user.email = compose(
+            trim,
+            normalizeEmail
+        )(user.email);
+
+        user.password = trim(user.password);
+    });
+
+    User.beforeCreate(async (user, options) => {
+        user.email = normalizeEmail(user.email) as string;
+        // // hash here pls
+        try {
+            user.password = await hash(user.password, 12);
+        } catch (e) {
+            console.error('Hashing failed. Pls handle error');
+        }
     });
 
     return User;
