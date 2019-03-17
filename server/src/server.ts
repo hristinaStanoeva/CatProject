a28383a337dc2d74f3812f7955f0b3e4040840f3
Petar Metodiev <petar.metodiev@gmail.com>
2019-03-17 09:55:25 +0200

    Stop resetting db for now

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,40 +6,39 @@ dotenv.config({ path: envFile });
 
 import app from './app';
 import { db } from './util/database';
-import { ListInstance, UserInstance } from './schemas';
 
 const anounceOpenPort = (port: number | string) => () =>
     console.log(`Listening on port ${port}`);
 const appPort = process.env.PORT || 3000;
-let userInstance: UserInstance;
+// let userInstance: UserInstance;
 // let listInstance: ListInstance;
 db.sequelize
     // .authenticate()
-    .sync({ force: true })
-    .then(() =>
-        db.User.create({
-            email: 'kote1@mail.com',
-            password: '12345678',
-        })
-    )
-    .then(user => {
-        userInstance = user;
-        return user.createList({
-            title: 'test list',
-        });
-    })
-    .then(async list => {
-        const li = db.ListItem.build({
-            content: 'todo',
-            checked: false,
-        });
-        await li.setAuthor(userInstance, { save: false });
-        await li.setList(list, { save: false });
-        return li.save();
-    })
-    .then(() => userInstance.isPasswordValid('12345678'))
+    .sync(/* { force: true } */)
+    // .then(() =>
+    //     db.User.create({
+    //         email: 'kote1@mail.com',
+    //         password: '12345678',
+    //     })
+    // )
+    // .then(user => {
+    //     userInstance = user;
+    //     return user.createList({
+    //         title: 'test list',
+    //     });
+    // })
+    // .then(async list => {
+    //     const li = db.ListItem.build({
+    //         content: 'todo',
+    //         checked: false,
+    //     });
+    //     await li.setAuthor(userInstance, { save: false });
+    //     await li.setList(list, { save: false });
+    //     return li.save();
+    // })
+    // .then(() => userInstance.isPasswordValid('12345678'))
     .then(res => {
-        console.log(res);
+        // console.log(res);
         console.log('Connected!');
         app.listen(appPort, anounceOpenPort(appPort));
     })
