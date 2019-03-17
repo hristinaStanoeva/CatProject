68f429d47e18f4225d2083db815a7c41d1fa5243
Petar Metodiev <petar.metodiev@gmail.com>
2019-03-17 09:49:57 +0200

    Add global error handler

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import bodyParser from 'body-parser';
 
 import apiRoutes from './routes/api.routes';
+import { NextFunction, Response, Request } from 'express';
+import { OperationalError } from './util/errors';
 
 const app = express();
 
@@ -11,5 +13,20 @@ app.use('/api', apiRoutes);
 app.use('*', (req, res) => {
     return res.sendStatus(404);
 });
+
+app.use(
+    (
+        err: OperationalError | Error,
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ) => {
+        if (err instanceof OperationalError) {
+            return res.status(err.statusCode).json({ messages: err.errorMessages });
+        }
+        console.log(err);
+        return res.status(500).json({ message: 'Server error' });
+    }
+);
 
 export default app;
