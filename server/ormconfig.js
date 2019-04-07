d07433b1413f525a490244b370538425652042a8
Petar Metodiev <petar.metodiev@gmail.com>
2019-04-07 13:48:29 +0300

    Move some config from ormconfig to database.ts

--- server/ormconfig.js
+++ server/ormconfig.js
@@ -9,11 +9,6 @@ module.exports = {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    synchronize: false,
-    logging: true,
-    entities: ['src/entities/**/*.entity.ts'],
-    migrations: ['src/migrations/**/*.migration.ts'],
-    subscribers: ['src/subscribers/**/*.subscriber.ts'],
     cli: {
         entitiesDir: 'src/entities',
         migrationsDir: 'src/migrations',
