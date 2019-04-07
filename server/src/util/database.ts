636c1011aeeac52f58505b60708d6d26dae00145
Petar Metodiev <petar.metodiev@gmail.com>
2019-04-07 13:48:29 +0300

    Move some config from ormconfig to database.ts

--- server/src/util/database.ts
+++ server/src/util/database.ts
@@ -1,42 +1,19 @@
-// import Sequelize from 'sequelize';
-// import {
-//     UserInstance,
-//     UserAttributes,
-//     UserFactory,
-//     ListInstance,
-//     ListAttributes,
-//     ListFactory,
-//     ListItemInstance,
-//     ListItemAttributes,
-//     ListItemFactory,
-// } from '../schemas';
-//
-// export interface Db {
-//     sequelize: Sequelize.Sequelize;
-//     Sequelize: Sequelize.SequelizeStatic;
-//     User: Sequelize.Model<UserInstance, UserAttributes>;
-//     List: Sequelize.Model<ListInstance, ListAttributes>;
-//     ListItem: Sequelize.Model<ListItemInstance, ListItemAttributes>;
-// }
-//
-// const sequelize = new Sequelize(process.env.DB_URL, {
-//     operatorsAliases: false,
-//     logging: false,
-//     define: { underscored: true },
-// });
-//
-// const dbConstruct: Db = {
-//     sequelize,
-//     Sequelize,
-//     User: UserFactory(sequelize, Sequelize),
-//     List: ListFactory(sequelize, Sequelize),
-//     ListItem: ListItemFactory(sequelize, Sequelize),
-// };
-//
-// Object.keys(dbConstruct).forEach(modelName => {
-//     if (dbConstruct[modelName].associate) {
-//         dbConstruct[modelName].associate(dbConstruct);
-//     }
-// });
-//
-// export const db = dbConstruct;
+import {
+    createConnection,
+    getConnectionOptions,
+    ConnectionOptions,
+} from 'typeorm';
+
+import { User } from '../entities/User.entity';
+
+export const db = ({ synchronize = false, logging = true } = {}) =>
+    getConnectionOptions().then((options: ConnectionOptions) =>
+        createConnection({
+            ...options,
+            entities: [User],
+            subscribers: [],
+            migrations: [],
+            synchronize,
+            logging
+        })
+    );
