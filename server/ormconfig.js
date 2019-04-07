0a0dc48759df54dd0284548fc4a480eb7a2b27b4
Petar Metodiev <petar.metodiev@gmail.com>
2019-04-07 23:35:51 +0300

    Add some missing fields in ormconfig.js
    Hopefully a step towards proper migrations.

--- server/ormconfig.js
+++ server/ormconfig.js
@@ -9,6 +9,10 @@ module.exports = {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    // next three entries are needed for cli migration generation
+    entities: ['dist/entities/**/*.entity.js'],
+    migrations: ['dist/migrations/**/*.js'],
+    subscribers: ['dist/subscribers/**/*.subscriber.js'],
     cli: {
         entitiesDir: 'src/entities',
         migrationsDir: 'src/migrations',
