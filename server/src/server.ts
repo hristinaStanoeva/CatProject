2f4ade6fa0efeabb5b77be3b413d1da959650e6d
Petar Metodiev <petar.metodiev@gmail.com>
2019-04-07 23:39:39 +0300

    Play around with creation and updating user entities

--- server/src/server.ts
+++ server/src/server.ts
@@ -5,14 +5,22 @@ import { Connection } from 'typeorm';
 
 import app from './app';
 import { db } from './util/database';
+import { getUserRepository } from './entities';
 
 const anounceOpenPort = (port: number | string) => () =>
     console.log(`Listening on port ${port}`);
 const appPort = process.env.PORT || 3000;
 
-db()
-    .then((connection: Connection) => {
+db({ synchronize: true })
+    .then(async (connection: Connection) => {
         console.log('Connected! ');
+        const user = getUserRepository().create({
+            email: 'koot@mail.com',
+            password: '1234567890',
+        });
+        await getUserRepository().save(user);
+        // const user = await getUserRepository().findOne({ email: 'kote112@mail.com' });
+        // await getUserRepository().update(user.id, { email: 'kotkot@mail.com' });
         app.listen(appPort, anounceOpenPort(appPort));
     })
     .catch(err => console.log('CONNECTION ERROR: ', err));
