7c1b510f4b2bf9b55159edf7479b9b498f6b5eb2
Petar Metodiev <petar.metodiev@gmail.com>
2019-05-01 18:50:13 +0300

    Run tests even if ts errors are present in the app
    And run prettier.

--- server/jest.config.js
+++ server/jest.config.js
@@ -2,22 +2,25 @@
 // https://jestjs.io/docs/en/configuration.html
 
 module.exports = {
-	// Automatically clear mock calls and instances between every test
-	clearMocks: true,
+    // Automatically clear mock calls and instances between every test
+    clearMocks: true,
 
-	// The directory where Jest should output its coverage files
-	coverageDirectory: "coverage",
+    // The directory where Jest should output its coverage files
+    coverageDirectory: 'coverage',
 
-	// A set of global variables that need to be available in all test environments
-	globals: {
-		'ts-jest': {
-			tsConfig: './__tests__/tsconfig.json'
-		}
-	},
+    // A set of global variables that need to be available in all test environments
+    globals: {
+        'ts-jest': {
+            tsConfig: './__tests__/tsconfig.json',
+            diagnostics: {
+                warnOnly: true,
+            },
+        },
+    },
 
-	// A preset that is used as a base for Jest's configuration
-	preset: 'ts-jest',
+    // A preset that is used as a base for Jest's configuration
+    preset: 'ts-jest',
 
-	// The test environment that will be used for testing
-	testEnvironment: "node",
+    // The test environment that will be used for testing
+    testEnvironment: 'node',
 };
