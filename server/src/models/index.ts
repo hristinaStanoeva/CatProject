16b9cbb65940959032599b0270ad96e3be2d1769
Petar Metodiev <petar.metodiev@gmail.com>
2019-05-05 16:47:38 +0300

    Add some clarifying comments on model files structure

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -3,3 +3,8 @@ export * from './custom-body-request.model';
 export * from './custom-locals-response.model';
 export * from './middleware.model';
 export * from './http-error.model';
+
+// create more structured model files, maybe:
+// http.models.ts - definition of custom body request and custom locals response, ResponseWithUser, LoginRequest, RegisterRequest, Errors, etc...
+// database.model.ts - base attributes
+// middleware.model.ts
